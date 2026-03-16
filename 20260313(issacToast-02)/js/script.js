abc86ad081e46e8da52aa280a80267a93f1209fb
Haeryun95 <haeryun805@gmail.com>
2026-03-16 10:44:04 +0900

    20260316

--- 20260313(issacToast-02)/js/script.js
+++ 20260313(issacToast-02)/js/script.js
@@ -1,8 +1,8 @@
-$(function () {
-    $("nav").mouseenter(function () {
-        $(".header").addClass("down");
-    });
-    $("nav").mouseleave(function () {
-        $(".header").removeClass("down");
-    });
-});
+$(function(){
+    $('nav').mouseenter(function(){
+        $('.header').addClass('down')
+    })
+    $('nav').mouseleave(function(){
+        $('.header').removeClass('down')
+    })
+})
