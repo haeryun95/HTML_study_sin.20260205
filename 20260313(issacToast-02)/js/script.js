6677dd9c44cbf46ee831c4b0e95b4310061f7463
Haeryun95 <haeryun805@gmail.com>
2026-03-17 14:55:07 +0900

    20260317

--- 20260313(issacToast-02)/js/script.js
+++ 20260313(issacToast-02)/js/script.js
@@ -1,8 +1,48 @@
-$(function(){
-    $('nav').mouseenter(function(){
-        $('.header').addClass('down')
-    })
-    $('nav').mouseleave(function(){
-        $('.header').removeClass('down')
-    })
-})
+$(function () {
+    $("nav").mouseenter(function () {
+        $(".header").addClass("down");
+    });
+    $("nav").mouseleave(function () {
+        $(".header").removeClass("down");
+    });
+});
+
+//visual swiper slider//
+
+const visualSwiper = new Swiper(".sw-visual", {
+    loop: true,
+    speed: 2000,
+    effect: "fade",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+const menuSwiper = new Swiper(".sw-menu", {
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
+    slidePrtView: 4,
+    navigation: {
+        prevEl: ".btn-prev",
+        nextEl: ".btn-next",
+    },
+    breakpoints: {
+        1281: {
+            //1281 픽셀 이상일 때
+            slidePrtView: 4,
+        },
+        681: {
+            //681 픽셀 이상일 때
+            slidePrtView: 4,
+        },
+        361: {
+            //361 픽셀 이상일 때
+            slidePrtView: 4,
+        },
+    },
+});
